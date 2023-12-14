2b3d660054c9c92cd9684073ed3900e8063d2894
SATU_dada <sttbjgn@gmail.com>
2023-12-14 21:30:54 +0545

    Few changes

---
+++ Assets/AttackingSystem_Scripts/ShipCategorizer_Size.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShipCategorizer_Size : MonoBehaviour
+{
+    public enum ShipSize
+    {
+        Small,Medium,Large
+    }
+    public ShipSize shipSize;
+}
